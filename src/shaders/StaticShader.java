645d736b348fc4aafa824f99d13c61994b40efd1
BuildTools <unconfigured@null.spigotmc.org>
2017-01-03 01:02:15 +0200

    Fixed input to StaticShader.

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -5,7 +5,7 @@ public class StaticShader extends ShaderProgram
     private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
 
-    public StaticShader(String vertexFile, String fragmentFile) {
+    public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
     }
 

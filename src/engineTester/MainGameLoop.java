64c21c48bb78ec3ce3fc167db7d413e0894de37a
BuildTools <unconfigured@null.spigotmc.org>
2017-01-03 01:01:46 +0200

    Added shader loading to Gameloop

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,14 +6,15 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
     public static void main(String[] args)
     {
         DisplayManager.createDisplay();
-
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
 
         float[] vertices = {
                 -0.5f, 0.5f, 0,
@@ -31,10 +32,13 @@ public class MainGameLoop {
         while(!Display.isCloseRequested())
         {
             renderer.prepare();
+            shader.start();
             renderer.render(model);
+            shader.stop();
             DisplayManager.updateDisplay();
         }
 
+        shader.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
     }

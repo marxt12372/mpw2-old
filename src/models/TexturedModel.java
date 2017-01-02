bbb26fc8cbca5dbf71f342f1ab5733c997961ff8
BuildTools <unconfigured@null.spigotmc.org>
2017-01-03 01:29:31 +0200

    Added TexturedModel.java and ModelTexture.java

---
+++ src/models/TexturedModel.java
@@ -0,0 +1,23 @@
+package models;
+
+import textures.ModelTexture;
+
+public class TexturedModel
+{
+    private RawModel rawModel;
+    private ModelTexture texture;
+
+    public TexturedModel(RawModel model, ModelTexture texture)
+    {
+        this.rawModel = model;
+        this.texture = texture;
+    }
+
+    public RawModel getRawModel() {
+        return rawModel;
+    }
+
+    public void setRawModel(RawModel rawModel) {
+        this.rawModel = rawModel;
+    }
+}

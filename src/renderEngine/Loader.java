22c2ecba7966fb0d6ced0aea26e6e656afe95510
BuildTools <unconfigured@null.spigotmc.org>
2017-01-03 16:39:15 +0200

    Started adding support for dynamic lighting

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -23,12 +23,13 @@ public class Loader {
     private List<Integer> vbos = new ArrayList<Integer>();
     private List<Integer> textures = new ArrayList<Integer>();
 
-    public RawModel loadToVAO(float[] positions, float[] textureCords, int[] indices)
+    public RawModel loadToVAO(float[] positions, float[] textureCords, float[] normals, int[] indices)
     {
         int vaoID = createVAO();
         bindIndicesBuffer(indices);
         storeDataInAttributeList(0, 3, positions);
         storeDataInAttributeList(1, 2, textureCords);
+        storeDataInAttributeList(2, 3, normals);
         unbindVAO();
         return new RawModel(vaoID, indices.length);
     }
